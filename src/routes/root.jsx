f05471426b07b4f7149edca16e896c25ffbd5011
James <>
2023-08-02 19:50:10 -0500

    added components folder, child route structure for root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from "react-router-dom";
 const Root = () => {
     return (
         <div>
-        Setting up react router scaffolding
+        <Outlet />
         </div>
     )
 }
