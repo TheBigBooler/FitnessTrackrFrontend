faa825f28d761e9d502fe205aa7e7f0eef732e90
James <>
2023-08-02 19:19:38 -0500

    react router scaffolding set up, tailwind set up

---
+++ src/routes/root.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <div>
+        Setting up react router scaffolding
+        </div>
+    )
+}
+
+export default Root;
