647eb0a26ecebc627d3691deaddc35bb3c636259
James <>
2023-08-02 22:07:51 -0500

    added global state to root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../components/NavBar";
 import { Outlet } from "react-router-dom";
-import { Link, NavLink } from "react-router-dom";
-
 
 const Root = () => {
+     const [isLoggedIn, setIsLoggedIn] = useState(false);
+     const [user, setUser] = useState("");
+     const [token, setToken] = useState("");
     return (
       <>
-      <Navigation/>
+        <Navigation
+        //props to be able to change login button to logout button, and clear user state
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          setToken={setToken}
+          setUser={setUser}
+        />
         <div>
-          <Outlet />
+          <Outlet
+          //props to be shared between outlet routes
+            context={{
+              isLoggedIn,
+              setIsLoggedIn,
+              user,
+              setUser,
+              token,
+              setToken,
+            }}
+          />
         </div>
       </>
     );
