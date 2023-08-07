f10a8f6e6f49faccc2c0ace10a5e125e3eb3655b
James <>
2023-08-07 18:33:56 -0500

    delete entire routine working

--- src/components/RoutinesByUser.jsx
+++ src/components/RoutinesByUser.jsx
@@ -83,6 +83,33 @@ const RoutinesByUser = () => {
               key={routine.id}
               className="m-3 border-red-200 bg-indigo-200 rounded border-2"
             >
+              <button
+                className="underline ml-2"
+                onClick={async () => {
+                  try {
+                    const response = await fetch(
+                      `${API_URL}routines/${routine.id}`,
+                      {
+                        method: "DELETE",
+                        headers: {
+                          "Content-Type": "application/json",
+                          Authorization: `Bearer ${token}`,
+                        },
+                      }
+                    );
+                    const result = await response.json();
+                    console.log(result);
+                    return result;
+                  } catch (err) {
+                    alert("Routine deletion unsuccessful");
+                    console.error(err);
+                  } finally {
+                    getRoutinesByUser();
+                  }
+                }}
+              >
+              -delete routine-
+              </button>
               <h3 className="text-2xl">{routine.name}</h3>
               <h4>
                 {routine.goal} - created by {routine.creatorName}
@@ -97,29 +124,33 @@ const RoutinesByUser = () => {
                         <span>for {activity.duration} minutes</span>
                       </p>
                       <p className="ml-3">{activity.description}</p>
-                      <button className="underline ml-3 mb-2"
-                      onClick={ async () => {
-                        try {
-                          const response = await fetch(
-                            `${API_URL}routine_activities/${activity.routineActivityId}`,
-                            {
-                              method: "DELETE",
-                              headers: {
-                                "Content-Type": "application/json",
-                                Authorization: `Bearer ${token}`,
-                              },
-                            }
-                          );
-                          const result = await response.json();
-                          console.log(result);
-                          return result;
-                        } catch (err) {
-                          alert("Activity removal unsuccessful")
-                          console.error(err);
-                        } finally {
-                          getRoutinesByUser();
-                        }
-                      }}>remove {activity.name} from {routine.name}</button>
+                      <button
+                        className="underline ml-3 mb-2"
+                        onClick={async () => {
+                          try {
+                            const response = await fetch(
+                              `${API_URL}routine_activities/${activity.routineActivityId}`,
+                              {
+                                method: "DELETE",
+                                headers: {
+                                  "Content-Type": "application/json",
+                                  Authorization: `Bearer ${token}`,
+                                },
+                              }
+                            );
+                            const result = await response.json();
+                            console.log(result);
+                            return result;
+                          } catch (err) {
+                            alert("Activity removal unsuccessful");
+                            console.error(err);
+                          } finally {
+                            getRoutinesByUser();
+                          }
+                        }}
+                      >
+                        remove {activity.name} from {routine.name}
+                      </button>
                     </>
                   );
                 })}
